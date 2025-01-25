fc8e9cef8ed947a98923359dcbd01d31297de3a6
Junghyun Kim <zerocool@sulhee.com>
2025-01-25 06:25:24 +0000

    API 예제 코드 추가

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -31,6 +31,24 @@ impl NeisClient {
 
     /// 학교기본정보
     /// https://open.neis.go.kr/portal/data/service/selectServicePage.do?page=1&rows=10&sortColumn=&sortDirection=&infId=OPEN17020190531110010104913&infSeq=2
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use neis_client::{types::SchoolInfoParams, Error, NeisClient};
+    ///
+    /// # async fn foo() -> Result<(), Error> {
+    /// let api_key = std::env::var("NEIS_API_KEY").unwrap();
+    /// let client = NeisClient::new(&api_key);
+    ///
+    /// let params = SchoolInfoParams {
+    ///     SD_SCHUL_CODE: Some(String::from("7031115")),
+    ///     ..Default::default()
+    /// };
+    /// let items = client.school_info(params).await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn school_info(
         &self,
         params: SchoolInfoParams,
@@ -79,6 +97,21 @@ impl NeisClient {
 
     /// 학급정보
     /// https://open.neis.go.kr/portal/data/service/selectServicePage.do?page=1&rows=10&sortColumn=&sortDirection=&infId=OPEN15320190408174919197546&infSeq=2
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use neis_client::{types::ClassInfoParams, Error, NeisClient};
+    ///
+    /// # async fn foo() -> Result<(), Error> {
+    /// let api_key = std::env::var("NEIS_API_KEY").unwrap();
+    /// let client = NeisClient::new(&api_key);
+    ///
+    /// let params = ClassInfoParams::new("B10", "7031115");
+    /// let items = client.class_info(params).await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn class_info(&self, params: ClassInfoParams) -> Result<Vec<ClassInfoItem>, Error> {
         let mut page = 1;
         let page_size = 1000;
@@ -124,6 +157,21 @@ impl NeisClient {
 
     /// 학교학과정보
     /// https://open.neis.go.kr/portal/data/service/selectServicePage.do?page=1&rows=10&sortColumn=&sortDirection=&infId=OPEN14020190311111456561190&infSeq=2
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use neis_client::{types::SchoolMajorInfoParams, Error, NeisClient};
+    ///
+    /// # async fn foo() -> Result<(), Error> {
+    /// let api_key = std::env::var("NEIS_API_KEY").unwrap();
+    /// let client = NeisClient::new(&api_key);
+    ///
+    /// let params = SchoolMajorInfoParams::new("B10").school_code("7031115");
+    /// let items = client.school_major_info(params).await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn school_major_info(
         &self,
         params: SchoolMajorInfoParams,
@@ -172,6 +220,21 @@ impl NeisClient {
 
     /// 학교계열정보
     /// https://open.neis.go.kr/portal/data/service/selectServicePage.do?page=1&rows=10&sortColumn=&sortDirection=&infId=OPEN13920190311110530306647&infSeq=2
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use neis_client::{types::SchoolAflcoInfoParams, Error, NeisClient};
+    ///
+    /// # async fn foo() -> Result<(), Error> {
+    /// let api_key = std::env::var("NEIS_API_KEY").unwrap();
+    /// let client = NeisClient::new(&api_key);
+    ///
+    /// let params = SchoolAflcoInfoParams::new("B10").school_code("7031115");
+    /// let items = client.school_aflco_info(params).await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn school_aflco_info(
         &self,
         params: SchoolAflcoInfoParams,
@@ -220,6 +283,21 @@ impl NeisClient {
 
     /// 학사일정
     /// https://open.neis.go.kr/portal/data/service/selectServicePage.do?page=1&rows=10&sortColumn=&sortDirection=&infId=OPEN17220190722175038389180&infSeq=2
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use neis_client::{types::SchoolScheduleParams, Error, NeisClient};
+    ///
+    /// # async fn foo() -> Result<(), Error> {
+    /// let api_key = std::env::var("NEIS_API_KEY").unwrap();
+    /// let client = NeisClient::new(&api_key);
+    ///
+    /// let params = SchoolScheduleParams::new("B10", "7031115");
+    /// let items = client.school_schedule(params).await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn school_schedule(
         &self,
         params: SchoolScheduleParams,
@@ -268,6 +346,21 @@ impl NeisClient {
 
     /// 초등학교시간표
     /// https://open.neis.go.kr/portal/data/service/selectServicePage.do?page=1&rows=10&sortColumn=&sortDirection=&infId=OPEN15020190408160341416743&infSeq=2
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use neis_client::{types::ElsTimetableParams, Error, NeisClient};
+    ///
+    /// # async fn foo() -> Result<(), Error> {
+    /// let api_key = std::env::var("NEIS_API_KEY").unwrap();
+    /// let client = NeisClient::new(&api_key);
+    ///
+    /// let params = ElsTimetableParams::new("B10", "7031115");
+    /// let items = client.els_timetable(params).await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn els_timetable(
         &self,
         params: ElsTimetableParams,
@@ -316,6 +409,21 @@ impl NeisClient {
 
     /// 중학교시간표
     /// https://open.neis.go.kr/portal/data/service/selectServicePage.do?page=1&rows=10&sortColumn=&sortDirection=&infId=OPEN15120190408165334348844&infSeq=2
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use neis_client::{types::MisTimetableParams, Error, NeisClient};
+    ///
+    /// # async fn foo() -> Result<(), Error> {
+    /// let api_key = std::env::var("NEIS_API_KEY").unwrap();
+    /// let client = NeisClient::new(&api_key);
+    ///
+    /// let params = MisTimetableParams::new("D10", "7261011");
+    /// let items = client.mis_timetable(params).await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn mis_timetable(
         &self,
         params: MisTimetableParams,
@@ -364,6 +472,21 @@ impl NeisClient {
 
     /// 고등학교시간표
     /// https://open.neis.go.kr/portal/data/service/selectServicePage.do?page=1&rows=10&sortColumn=&sortDirection=&infId=OPEN18620200826103326268120&infSeq=2
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use neis_client::{types::HisTimetableParams, Error, NeisClient};
+    ///
+    /// # async fn foo() -> Result<(), Error> {
+    /// let api_key = std::env::var("NEIS_API_KEY").unwrap();
+    /// let client = NeisClient::new(&api_key);
+    ///
+    /// let params = HisTimetableParams::new("D10", "7240090");
+    /// let items = client.his_timetable(params).await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn his_timetable(
         &self,
         params: HisTimetableParams,
@@ -412,6 +535,21 @@ impl NeisClient {
 
     /// 특수학교시간표
     /// https://open.neis.go.kr/portal/data/service/selectServicePage.do?page=1&rows=10&sortColumn=&sortDirection=&infId=OPEN18520200826093359591792&infSeq=2
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use neis_client::{types::SpsTimetableParams, Error, NeisClient};
+    ///
+    /// # async fn foo() -> Result<(), Error> {
+    /// let api_key = std::env::var("NEIS_API_KEY").unwrap();
+    /// let client = NeisClient::new(&api_key);
+    ///
+    /// let params = SpsTimetableParams::new("B10", "7010575");
+    /// let items = client.sps_timetable(params).await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn sps_timetable(
         &self,
         params: SpsTimetableParams,
@@ -460,6 +598,21 @@ impl NeisClient {
 
     /// 시간표강의실정보
     /// https://open.neis.go.kr/portal/data/service/selectServicePage.do?page=2&rows=10&sortColumn=&sortDirection=&infId=OPEN14120190311112536362172&infSeq=2
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use neis_client::{types::ClassRoomInfoParams, Error, NeisClient};
+    ///
+    /// # async fn foo() -> Result<(), Error> {
+    /// let api_key = std::env::var("NEIS_API_KEY").unwrap();
+    /// let client = NeisClient::new(&api_key);
+    ///
+    /// let params = ClassRoomInfoParams::new("B10", "7010575");
+    /// let items = client.class_room_info(params).await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn class_room_info(
         &self,
         params: ClassRoomInfoParams,
@@ -508,6 +661,21 @@ impl NeisClient {
 
     /// 학원교습소정보
     /// https://open.neis.go.kr/portal/data/service/selectServicePage.do?page=1&rows=10&sortColumn=&sortDirection=&infId=OPEN19220231012134453534385&infSeq=2
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use neis_client::{types::AcademyInfoParams, Error, NeisClient};
+    ///
+    /// # async fn foo() -> Result<(), Error> {
+    /// let api_key = std::env::var("NEIS_API_KEY").unwrap();
+    /// let client = NeisClient::new(&api_key);
+    ///
+    /// let params = AcademyInfoParams::new("D10");
+    /// let items = client.academy_info(params).await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn academy_info(
         &self,
         params: AcademyInfoParams,
@@ -556,6 +724,21 @@ impl NeisClient {
 
     /// 급식식단정보
     /// https://open.neis.go.kr/portal/data/service/selectServicePage.do?page=1&rows=10&sortColumn=&sortDirection=&infId=OPEN17320190722180924242823&infSeq=2
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use neis_client::{types::MealServiceParams, Error, NeisClient};
+    ///
+    /// # async fn foo() -> Result<(), Error> {
+    /// let api_key = std::env::var("NEIS_API_KEY").unwrap();
+    /// let client = NeisClient::new(&api_key);
+    ///
+    /// let params = MealServiceParams::new("B10", "7031115").from_ymd(2025, 1, 1);
+    /// let items = client.meal_service(params).await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn meal_service(
         &self,
         params: MealServiceParams,
