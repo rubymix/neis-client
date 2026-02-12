83f6df9ec0b9ba14902973fa074ed8fa2ac13864
Junghyun Kim <zerocool@sulhee.com>
2026-02-13 05:41:53 +0900

    Error 에 std::error::Error 구현 추가

--- src/error.rs
+++ src/error.rs
@@ -16,6 +16,8 @@ impl Error {
     }
 }
 
+impl std::error::Error for Error {}
+
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match *self {
